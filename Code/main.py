3ec4a7baf08f417383f91c2fec9184ad6e546152
ViniiSalles <yvinilordsalles@gmail.com>
2024-08-19 12:14:04 -0300

    add classes

---
+++ Code/main.py
@@ -0,0 +1,16 @@
+from Classes.aluno import Aluno
+from Classes.professor import Professor
+from Classes.secretario import Secretario
+
+
+print(
+    "-" * 30 + " SISTEMA DE MATRICULAS " + "-" * 30 + "\n" +
+    "|                                                                                 |\n" +
+    "|  1. Aluno                                                                       |\n" +
+    "|  2. Professor                                                                   |\n" +
+    "|  3. Secretario                                                                  |\n" +
+    "|  4. Sair                                                                        |\n" +
+    "|                                                                                 |\n" +
+    "-" * 83
+)
+ 
